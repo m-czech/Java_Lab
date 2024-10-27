1a7315351ff148cfe14910f183591b995a4d534a
Mateusz Czech <mateusz.czech@student.pk.edu.pl>
2024-10-27 11:19:18 +0100

    Ex1

---
+++ Lab1/src/Ex1/Shape.java
@@ -0,0 +1,15 @@
+package Ex1;
+
+// Polecenia użyte do kompilacji i wykonania
+// > javac -d . Shape.java
+// > java Ex1.Shape
+
+public class Shape {
+    public static void main(String[] args) {
+        print();
+    }
+
+    public static void print() {
+        System.out.println("Shape");
+    }
+}
