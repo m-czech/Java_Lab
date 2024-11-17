d0730dfc99378cbd014cfea86ca0f536e3d19d47
Mateusz Czech <mateusz.czech@student.pk.edu.pl>
2024-11-17 12:11:02 +0100

    Ex3

--- Lab3/src/main/java/org/example/Main.java
+++ Lab3/src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
 public class Main {
-
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(1, 1, new Color(1,2,3));
         Triangle triangle = new Triangle(1,2,3, 4, new Color(1,2,3));
+
+        ShapeDescriber shapeDescriber = new ShapeDescriber();
+        shapeDescriber.describe(rectangle);
+        shapeDescriber.describe(triangle);
     }
 }
--- Lab3/src/main/java/org/example/ShapeDescriber.java
+++ Lab3/src/main/java/org/example/ShapeDescriber.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ShapeDescriber {
+    private final Logger _logger;
+
+    public ShapeDescriber() {
+        this._logger = LoggerFactory.getLogger(ShapeDescriber.class);
+    }
     public void describe(Shape shape) {
+        _logger.info("test");
         System.out.println("Area: " + shape.getArea());
         System.out.println("Perimeter: " + shape.getPerimeter());
     }
